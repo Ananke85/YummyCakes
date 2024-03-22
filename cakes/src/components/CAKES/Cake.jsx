dadb81155d85ba01a446a3445f0c1493edbd81bb
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2024-03-22 18:20:49 +0100

    FEAT(Global): Starting with responsiveness

--- cakes/src/components/CAKES/Cake.jsx
+++ cakes/src/components/CAKES/Cake.jsx
@@ -4,19 +4,31 @@ import { getCakeById } from "../../utils/apiCakes";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Cake = ({ cakeDisplayed }) => {
+const Cake = ({ cakeDisplayed, rotation }) => {
   const id = cakeDisplayed?._id;
   const { data: cake } = useQuery(["cakes", id], getCakeById);
 
+  console.log("los cakes", cake)
+
+
+  const cakeTitle = cake && cake.title.replace(/\s+/g, '-');
+  console.log("el titulo", cakeTitle)
+
+
   return (
     <>
-      <div className={styles.chocoberryContainer}>
+      {/* <div className={styles.cakeContainer}> */}
+      <div
+        className={`${styles.cakeContainer} ${cakeTitle ? styles[cakeTitle] : ""}`}
+        
+      >
         {cake && (
           <div className={styles.cakeDetails}>
             <h1>{cake.title}</h1>
             <p>{cake.description}</p>
             <Link to={`/recipes/${cake.title}`}>View recipe</Link>
-            <img src={cake.image} alt={cake.title} />
+            <img src={cake.image} alt={cake.title} 
+            style={{ transform: `rotate(${rotation}deg)` }} />
           </div>
         )}
       </div>
@@ -47,6 +59,7 @@ Cake.propTypes = {
   cakeDisplayed: PropTypes.shape({
     _id: PropTypes.string.isRequired,
   }).isRequired,
+  rotation: PropTypes.number.isRequired
 };
 
 export default Cake;
--- cakes/src/components/Home/Home.jsx
+++ cakes/src/components/Home/Home.jsx
@@ -9,17 +9,27 @@ const Home = () => {
   const cakesLinks = cakes?.map((cake) => cake.title);
 
   const [currentCakeIndex, setCurrentCakeIndex] = useState(0);
+  const [rotation, setRotation] = useState(0)
   const currentCake = cakes && cakes[currentCakeIndex];
+
+  const handleNavItem = (index) => {
+    setCurrentCakeIndex(index);
+    setRotation(rotation + 360)
+  }
 
   const handlePrevious = () => {
     if (currentCakeIndex > 0) {
       setCurrentCakeIndex(currentCakeIndex - 1);
+      setRotation(rotation + 360)
     }
   };
 
   const handleNext = () => {
     if (currentCakeIndex < cakes?.length - 1) {
       setCurrentCakeIndex(currentCakeIndex + 1);
+      setRotation(rotation + 360)
+
+
     }
   };
 
@@ -28,25 +38,25 @@ const Home = () => {
       <div className={styles.homeContainer}>
         <div className={styles.navBar}>
           {cakes &&
-            cakesLinks.map((cake) => (
-              <div key={cake._id} className={styles.navItem}>
+            cakesLinks.map((cake, index) => (
+              <div key={index} className={styles.navItem} onClick={() => handleNavItem(index)}>
                 {cake}
               </div>
             ))}
         </div>
         <div className={styles.buttonsContainer}>
-
-          {currentCakeIndex !==0 && (
-          <button onClick={handlePrevious} className={styles.buttonLeft}>previous</button>
-
+          {currentCakeIndex !== 0 && (
+            <button onClick={handlePrevious} className={styles.buttonLeft}>
+              <span className="icon-arrow-back-outline"></span>
+            </button>
           )}
-          <div></div>
-          {currentCakeIndex !== cakes.length -1 && (
-          <button onClick={handleNext}>next</button>
-
+          {currentCakeIndex !== cakes?.length - 1 && (
+            <button onClick={handleNext} className={styles.buttonRight}>
+              <span className="icon-arrow-forward-outline"></span>
+            </button>
           )}
         </div>
-        {cakes && <Cake cakeDisplayed={currentCake} />}
+        {cakes && <Cake cakeDisplayed={currentCake} rotation={rotation} />}
       </div>
     </>
   );
