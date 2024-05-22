e1a8dca64f226c107d588f0d5a74930e5c13b2c0
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2024-05-22 12:34:50 +0200

    production

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
-const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary"); // Import CloudinaryStorage
+// const multer = require("multer");
+// const { CloudinaryStorage } = require("multer-storage-cloudinary"); // Import CloudinaryStorage
 
 const cakeRouter = require("./mongo/router/cakeRouter");
 const recipeRouter = require("./mongo/router/recipeRouter")
@@ -15,16 +15,16 @@ cloudinary.config({
 });
 
 // Use CloudinaryStorage with Multer
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "cakes", // Change this to your desired folder
-    format: async (req, file) => "png", // You can change the format if needed
-    public_id: (req, file) => file.fieldname + "-" + Date.now(),
-  },
-});
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: "cakes", // Change this to your desired folder
+//     format: async (req, file) => "png", // You can change the format if needed
+//     public_id: (req, file) => file.fieldname + "-" + Date.now(),
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 const app = express();
 app.use(cors());
 app.use(express.json());
