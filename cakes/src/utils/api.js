7cbecefffd09382a7791551092d4d9c5cfcc70b6
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2024-05-22 12:57:05 +0200

    deploy

--- cakes/src/utils/api.js
+++ cakes/src/utils/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+const URL_API =
+  window.location.hostname === "https://yum-yum-yummycakes.netlify.app"
+    ? "yummycakes-production.up.railway.app"
+    : "http://localhost:3005";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3005",
+  baseURL: URL_API,
+  // baseURL: "http://localhost:3005",
   headers: { "Content-Type": "application/json" },
 });
